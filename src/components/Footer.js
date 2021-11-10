5095d7f88cc6fc522875b15aaeed46664cedf0c6
GuMarYae <tddanielsjr@gmail.com>
2021-11-09 22:14:41 -0600

    pic and css check

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,45 @@
 function Footer(props) {
-    return <h1>Footer</h1>;
+    return <div class ="Footer">
+      <ul>
+    <h3>Email</h3>
+    <li>tddanielsjr@gmail.com</li>
+</ul>
+<ul><h3>Number</h3>
+    <li>413-693-8004</li>
+</ul>
+<ul><h3>Degree(s)</h3>
+    <li>BS Audio Engineering and tech</li>
+    <li>AS Electronic Engineering and tech</li>
+    <li>AA Fine Arts/ Science</li>
+</ul>
+<ul><h3>Hobbies</h3>
+    <li>Audio production</li>
+    <li>Drawing</li>
+    <li>Video production</li>
+    <li>Audio mixing</li>
+    <li>Audio mastering</li>
+    <li>Videogames</li>
+    <li>working out</li>
+    <li>Movies</li>
+    <li>Reeading</li>
+    <li>Watching sports</li>
+    <li>Traveling</li>
+    
+     <div class="siteLinks">
+     <li><a href = "https://www.linkedin.com/in/tony-daniels-8b6058165/">Linkedin</a></li>
+    <li><a href = "https://www.instagram.com/gumaryae/">instagram</a></li>
+    <li><a href = "https://www.facebook.com/tony.daniels.712">Facebook</a></li>
+    <li><a href = "    https://www.fiverr.com/gumaryae/get-your-recording-done-in-24-hours?utm_campaign=gigs_show&utm_medium=shared&utm_source=twitter&utm_term=vzmyvd">Fiverr</a></li>
+
+    <li><a href = "https://github.com/GuMarYae">Github</a></li>
+     </div>
+    
+</ul>
+      
+
+
+      
+    </div>;
   }
   
   export default Footer;
