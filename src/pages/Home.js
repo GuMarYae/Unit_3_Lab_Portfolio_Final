3108c779578e5c7d50a96f961dd10ad02092c303
GuMarYae <tddanielsjr@gmail.com>
2021-11-24 21:37:43 -0600

    added home pic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,11 @@
 function Home(props) {
-    return <h1>Home</h1>;
+    return (
+    <div class = "Home">
+    <h1>Home</h1>
+      <img src="https://media-exp1.licdn.com/dms/image/C4D03AQF6k6qCtXyv7g/profile-displayphoto-shrink_400_400/0/1621613564207?e=1643241600&v=beta&t=Hspz47H8IBfW61BWqSEgAR1OVTqrgUDroZviK-D3qS8"/>
+    </div>
+
+    );
   } 
   
   export default Home;
