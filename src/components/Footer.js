abdbf8e9b7bca82af634d2480ad7998926736fac
GuMarYae <tddanielsjr@gmail.com>
2021-11-24 21:46:43 -0600

    added pic and reordered the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,6 +14,16 @@ function Footer(props) {
     <li>AS Electronic Engineering and tech</li>
     <li>AA Fine Arts/ Science</li>
 </ul>
+
+<div class="siteLinks">
+     <li><a href = "https://www.linkedin.com/in/tony-daniels-8b6058165/">Linkedin</a></li>
+    <li><a href = "https://www.instagram.com/gumaryae/">instagram</a></li>
+    <li><a href = "https://www.facebook.com/tony.daniels.712">Facebook</a></li>
+    <li><a href = "https://www.fiverr.com/gumaryae/get-your-recording-done-in-24-hours?utm_campaign=gigs_show&utm_medium=shared&utm_source=twitter&utm_term=vzmyvd">Fiverr</a></li>
+
+    <li><a href = "https://github.com/GuMarYae">Github</a></li>
+     </div>
+
 <ul><h3>Hobbies</h3>
     <li>Audio production</li>
     <li>Drawing</li>
@@ -27,14 +37,7 @@ function Footer(props) {
     <li>Watching sports</li>
     <li>Traveling</li>
     
-     <div class="siteLinks">
-     <li><a href = "https://www.linkedin.com/in/tony-daniels-8b6058165/">Linkedin</a></li>
-    <li><a href = "https://www.instagram.com/gumaryae/">instagram</a></li>
-    <li><a href = "https://www.facebook.com/tony.daniels.712">Facebook</a></li>
-    <li><a href = "https://www.fiverr.com/gumaryae/get-your-recording-done-in-24-hours?utm_campaign=gigs_show&utm_medium=shared&utm_source=twitter&utm_term=vzmyvd">Fiverr</a></li>
-
-    <li><a href = "https://github.com/GuMarYae">Github</a></li>
-     </div>
+     
     
 </ul>
       
