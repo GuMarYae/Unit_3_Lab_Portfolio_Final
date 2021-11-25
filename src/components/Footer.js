4c3fcfce6a541478ac42cd3db49d0a46c1d0afc8
GuMarYae <tddanielsjr@gmail.com>
2021-11-24 21:58:24 -0600

    typos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,50 +1,65 @@
 function Footer(props) {
-    return <div class ="Footer">
+  return (
+    <div class="Footer">
       <ul>
-    <h3>Email</h3>
-    <li>tddanielsjr @gmail.com</li>
-   </ul>
-<ul><h3>Number</h3>
-    <li>413-693-8004</li>
-</ul>
-<ul><h3>Degree(s)/ certificates</h3>
-    <li>General Assembly software development
-        /engineering</li><br></br>
-    <li>BS Audio Engineering and tech</li>
-    <li>AS Electronic Engineering and tech</li>
-    <li>AA Fine Arts/ Science</li>
-</ul>
+        <h3>Email</h3>
+        <li>tddanielsjr @gmail.com</li>
+      </ul>
+      <ul>
+        <h3>Number</h3>
+        <li>413-693-8004</li>
+      </ul>
+      <ul>
+        <h3>Degree(s)/ certificates</h3>
+        <li>General Assembly software development /engineering</li>
+        <br></br>
+        <li>BS Audio Engineering and tech</li>
+        <li>AS Electronic Engineering and tech</li>
+        <li>AA Fine Arts/ Science</li>
+      </ul>
 
-<div class="siteLinks">
-     <li><a href = "https://www.linkedin.com/in/tony-daniels-8b6058165/">Linkedin</a></li>
-    <li><a href = "https://www.instagram.com/gumaryae/">instagram</a></li>
-    <li><a href = "https://www.facebook.com/tony.daniels.712">Facebook</a></li>
-    <li><a href = "https://www.fiverr.com/gumaryae/get-your-recording-done-in-24-hours?utm_campaign=gigs_show&utm_medium=shared&utm_source=twitter&utm_term=vzmyvd">Fiverr</a></li>
+      <div class="siteLinks">
+        <li>
+          <a href="https://www.linkedin.com/in/tony-daniels-8b6058165/">
+            Linkedin
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/gumaryae/">instagram</a>
+        </li>
+        <li>
+          <a href="https://www.facebook.com/tony.daniels.712">Facebook</a>
+        </li>
+        <li>
+          <a href="https://www.fiverr.com/gumaryae/get-your-recording-done-in-24-hours?utm_campaign=gigs_show&utm_medium=shared&utm_source=twitter&utm_term=vzmyvd">
+            Fiverr
+          </a>
+        </li>
 
-    <li><a href = "https://github.com/GuMarYae">Github</a></li>
-     </div>
+        <li>
+          <a href="https://github.com/GuMarYae">Github</a>
+        </li>
+      </div>
 
-<ul><h3>Hobbies</h3>
-    <li>Audio production</li>
-    <li>Drawing</li>
-    <li>Video production</li>
-    <li>Audio mixing</li>
-    <li>Audio mastering</li>
-    <li>Videogames</li>
-    <li>working out</li>
-    <li>Movies</li>
-    <li>Reeading</li>
-    <li>Watching sports</li>
-    <li>Traveling</li>
-    
-     
-    
-</ul>
-      
+      <ul>
+        <h3>Hobbies</h3>
+        <li>family and friends</li>
+        <li>coding</li>
+        <li>reading watching coding tutorials</li>
+        <li>Audio production</li>
+        <li>Audio mixing</li>
+        <li>Audio mastering</li>
+        <li>Watching sports</li>
+        <li>Traveling</li>
+        <li>Movies</li>
+        <li>working out</li>
+        <li>Videogames</li>
+        <li>Drawing</li>
+        <li>Video production</li>
+        <li>Reading</li>
+      </ul>
+    </div>
+  );
+}
 
-
-      
-    </div>;
-  }
-  
-  export default Footer;
+export default Footer;
