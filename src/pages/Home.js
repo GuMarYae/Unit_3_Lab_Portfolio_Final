1c34ab5a9c1cb459ab0c4d26ae1b9fe280e0135e
GuMarYae <tddanielsjr@gmail.com>
2021-12-05 15:58:13 -0600

    updated colors and attached resume

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ function Home(props) {
     <div class = "Home">
     <h1>Home</h1>
       <img src="https://media-exp1.licdn.com/dms/image/C4D03AQF6k6qCtXyv7g/profile-displayphoto-shrink_400_400/0/1621613564207?e=1643241600&v=beta&t=Hspz47H8IBfW61BWqSEgAR1OVTqrgUDroZviK-D3qS8"/>
+      
     </div>
 
     );
